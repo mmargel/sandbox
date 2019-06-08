3c0fff16ced256660fc3adfcfc4f1d097780c5d5
mmargel <michael.margel@shopify.com>
2019-06-08 14:42:49 -0400

    updated linked list

--- ruby/data_structures/linked_list.rb
+++ ruby/data_structures/linked_list.rb
@@ -1,100 +1,52 @@
 # frozen_string_literal: true
 
 # Standard Singly Linked List
+# @head = [*, -]--> [*, -]--> [*, -]--> nil
 class LinkedList
   def initialize
     @head = nil
-    @tail = nil
   end
 
-  def insert_head(item)
+  def insert(item)
     raise(ArgumentError, 'item cannot be nil') if item.nil?
     new_node = Node.new(item)
 
-    if @head.nil?
-      @head = new_node
-      @tail = new_node
-    else
-      @head.next = new_node
-      @head = new_node
-    end
+    new_node.next = @head unless @head.nil?
+    @head = new_node
 
     item
   end
 
-  def insert_tail(item)
-    raise(ArgumentError, 'item cannot be nil') if item.nil?
-    new_node = Node.new(item)
-
-    if @tail.nil?
-      @head = new_node
-    else
-      new_node.next = @tail
-    end
-
-    @tail = new_node
-    item
-  end
-
-  def remove_head
+  def remove
     raise(ArgumentError, 'list is empty') if empty?
 
     item = @head
-
-    if @head == @tail
-      @head = nil
-      @tail = nil
-    else
-      new_head = @tail
-      new_head = new_head.next while new_head.next != item
-      @head = new_head
-      @head.next = nil
-    end
+    @head = @head.next
     item.value
   end
 
-  def remove_tail
-    raise(ArgumentError, 'list is empty') if empty?
-
-    item = @tail
-    if @head == @tail
-      @head = nil
-      @tail = nil
-    else
-      @tail = @tail.next
-    end
-
-    item.value
-  end
-
-  def peek_head
+  def peek
     raise(ArgumentError, 'list is empty') if empty?
 
     @head.value
   end
 
-  def peek_tail
-    raise(ArgumentError, 'list is empty') if empty?
-
-    @tail.value
-  end
-
   def empty?
-    @head == @tail && @head.nil?
+    @head.nil?
   end
 
   def find(value)
-    target = @tail
+    target = @head
     target = target.next while !target.nil? && target.value != value
     target
   end
 
   def delete(value)
-    if @tail.value == value
-      match = @tail
-      @tail = @tail.next
+    if @head.value == value
+      match = @head
+      @head = @head.next
     else
-      pre_target = @tail
+      pre_target = @head
       pre_target = pre_target.next while !pre_target.nil? && !pre_target.next.nil? && pre_target.next.value != value
       match = pre_target.next
       return nil if match.nil?
--- ruby/test/data_structures/tc_linked_list_test.rb
+++ ruby/test/data_structures/tc_linked_list_test.rb
@@ -7,107 +7,47 @@ class TestLinkedList < Test::Unit::TestCase
     @list = LinkedList.new
   end
 
-  # insert_head
-  def test_insert_head_nil
-    assert_raise(ArgumentError) { @list.insert_head(nil) }
+  # insert
+  def test_insert_nil
+    assert_raise(ArgumentError) { @list.insert(nil) }
   end
 
-  def test_insert_head_non_nil
-    assert_equal(2, @list.insert_head(2))
-    assert_equal(4, @list.insert_head(4))
+  def test_insert_non_nil
+    assert_equal(2, @list.insert(2))
+    assert_equal(4, @list.insert(4))
   end
 
-  # insert_tail
-  def test_insert_tail_nil
-    assert_raise(ArgumentError) { @list.insert_tail(nil) }
+  # remove
+  def test_remove_empty
+    assert_raise(ArgumentError) { @list.remove(nil) }
   end
 
-  def test_insert_tail_non_nil
-    assert_equal(2, @list.insert_tail(2))
-    assert_equal(4, @list.insert_tail(4))
+  def test_remove_non_empty
+    @list.insert(3)
+    assert_equal(3, @list.remove)
   end
 
-  # remove_head
-  def test_remove_head_empty
-    assert_raise(ArgumentError) { @list.remove_head(nil) }
+  def test_remove_multi
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
+    assert_equal(3, @list.remove)
+    assert_equal(2, @list.remove)
+    assert_equal(1, @list.remove)
   end
 
-  def test_remove_head_non_empty
-    @list.insert_head(3)
-    assert_equal(3, @list.remove_head)
+
+  # peek
+  def test_peek_empty
+    assert_raise(ArgumentError) { @list.remove }
   end
 
-  def test_remove_head_multi
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
-    assert_equal(3, @list.remove_head)
-    assert_equal(2, @list.remove_head)
-    assert_equal(1, @list.remove_head)
-  end
-
-  # remove_tail
-  def test_remove_tail_empty
-    assert_raise(ArgumentError) { @list.remove_tail(nil) }
-  end
-
-  def test_remove_tail_non_empty
-    @list.insert_tail(3)
-    assert_equal(3, @list.remove_tail)
-  end
-
-  def test_remove_tail_multi
-    @list.insert_tail(1)
-    @list.insert_tail(2)
-    @list.insert_tail(3)
-    assert_equal(3, @list.remove_tail)
-    assert_equal(2, @list.remove_tail)
-    assert_equal(1, @list.remove_tail)
-  end
-
-  # head/tail mixed operations
-  def test_add_head_remove_tail
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
-    assert_equal(1, @list.remove_tail)
-    assert_equal(2, @list.remove_tail)
-    assert_equal(3, @list.remove_tail)
-  end
-
-  def test_add_tail_remove_head
-    @list.insert_tail(1)
-    @list.insert_tail(2)
-    @list.insert_tail(3)
-    assert_equal(1, @list.remove_head)
-    assert_equal(2, @list.remove_head)
-    assert_equal(3, @list.remove_head)
-  end
-
-  # peek_head
-  def test_peek_head_empty
-    assert_raise(ArgumentError) { @list.remove_head }
-  end
-
-  def test_peek_head_non_empty
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
-    assert_equal(3, @list.peek_head)
-    assert_equal(3, @list.peek_head)
-  end
-
-  # peek_tail
-  def test_peek_tail_empty
-    assert_raise(ArgumentError) { @list.peek_tail }
-  end
-
-  def test_peek_tail_non_empty
-    @list.insert_tail(1)
-    @list.insert_tail(2)
-    @list.insert_tail(3)
-    assert_equal(3, @list.peek_tail)
-    assert_equal(3, @list.peek_tail)
+  def test_peek_non_empty
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
+    assert_equal(3, @list.peek)
+    assert_equal(3, @list.peek)
   end
 
   # empty
@@ -116,54 +56,54 @@ class TestLinkedList < Test::Unit::TestCase
   end
 
   def test_empty_false
-    @list.insert_head(3)
+    @list.insert(3)
     assert(!@list.empty?)
   end
 
   def test_empty_dequeued
-    @list.insert_head(3)
-    @list.remove_tail
+    @list.insert(3)
+    @list.remove
     assert(@list.empty?)
   end
 
   # find
   def test_find_match
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
     assert_equal(2, @list.find(2).value)
   end
 
   def test_find_no_match
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
     assert_nil(@list.find(4))
   end
 
   # delete
   def test_delete_match
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
     assert_not_nil(@list.find(2))
     assert_equal(2, @list.delete(2).value)
     assert_nil(@list.find(2))
   end
 
   def test_delete_head
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
     assert_not_nil(@list.find(3))
     assert_equal(3, @list.delete(3).value)
     assert_nil(@list.find(3))
   end
 
   def test_delete_no_match
-    @list.insert_head(1)
-    @list.insert_head(2)
-    @list.insert_head(3)
+    @list.insert(1)
+    @list.insert(2)
+    @list.insert(3)
     assert_nil(@list.delete(4))
   end
 end
